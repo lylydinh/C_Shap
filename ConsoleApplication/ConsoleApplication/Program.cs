60492c47e11c732f965ac184097fb8aee4ee7d5c
THUY\THUYD6 <buithuydinh.epu@gmail.com>
2016-09-11 20:50:07 +0700

    Hello world Project

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        /// <summary>
+        /// Public la che do cong khai dug cho cac class khac co the tham chieu
+        /// Main la class khoi chay dau tien khi chay cua ung dung
+        /// void la phuong thuc khng co gia tri tra ve
+        /// static:
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            var name = "thuy bui";//var la khai bao bien no tuong duong voi
+            string name2 = "thuybui";//2 dong nay y nghix tuong duong nhua deu co ngia la khai ba 1 bien string
+            Console.WriteLine($"Hello {name}+{name2}");
+            Console.ReadLine();//lenh dung man hinh khi chay chuong trinh
+
+        }
+    }
+}
